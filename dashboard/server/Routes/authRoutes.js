9afcd51dd1f87999db3ece300204718438775aae
P4ver <s.pales20@gmail.com>
2024-09-16 01:21:21 +0100

    add routes for login and logout

--- dashboard/server/Routes/authRoutes.js
+++ dashboard/server/Routes/authRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const register = require('../Controller/authController');
+const {login, logout, register} = require('../Controller/authController');
 
 router.post('/register', register)
+router.post('/login', login)
+router.post('/logout', logout)
 
 module.exports = router;
 
