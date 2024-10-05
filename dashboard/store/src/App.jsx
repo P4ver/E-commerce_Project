10934b7c70a34e930d244e7fc9f51ada15ff13c7
P4ver <s.pales20@gmail.com>
2024-10-05 11:53:03 +0100

    add navbar

--- dashboard/store/src/App.jsx
+++ dashboard/store/src/App.jsx
@@ -1,11 +1,10 @@
 import './App.css'
+import ResponsiveAppBar from './component/NavBare'
 
 function App() {
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+<ResponsiveAppBar/>
 
     </>
   )
