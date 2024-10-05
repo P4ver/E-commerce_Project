acc9de9eaaf30129b3961bc782226a1af2dca78e
P4ver <s.pales20@gmail.com>
2024-10-05 11:46:45 +0100

    chart in dashboard

--- dashboard/backoffice/src/component/dashboard.jsx
+++ dashboard/backoffice/src/component/dashboard.jsx
@@ -1,8 +1,156 @@
 // src/pages/Dashboard.js
 import React from 'react';
+import { PieChart } from '@mui/x-charts/PieChart';
+import { BarChart } from '@mui/x-charts/BarChart';
+import { axisClasses } from '@mui/x-charts/ChartsAxis';
+export const desktopOS = [
+  {
+    label: 'Windows',
+    value: 72.72,
+  },
+  {
+    label: 'OS X',
+    value: 16.38,
+  },
+  {
+    label: 'Linux',
+    value: 3.83,
+  },
+  {
+    label: 'Chrome OS',
+    value: 2.42,
+  },
+  {
+    label: 'Other',
+    value: 4.65,
+  },
+];
 
+export const valueFormatterPie = (item) => `${item.value}%`;
+
+
+
+
+const chartSetting = {
+  yAxis: [
+    {
+      label: 'rainfall (mm)',
+    },
+  ],
+  width: 500,
+  height: 300,
+  sx: {
+    [`.${axisClasses.left} .${axisClasses.label}`]: {
+      transform: 'translate(-20px, 0)',
+    },
+  },
+};
+
+
+export const dataset = [
+  {
+    london: 59,
+    paris: 57,
+    newYork: 86,
+    seoul: 21,
+    month: 'Jan',
+  },
+  {
+    london: 50,
+    paris: 52,
+    newYork: 78,
+    seoul: 28,
+    month: 'Feb',
+  },
+  {
+    london: 47,
+    paris: 53,
+    newYork: 106,
+    seoul: 41,
+    month: 'Mar',
+  },
+  {
+    london: 54,
+    paris: 56,
+    newYork: 92,
+    seoul: 73,
+    month: 'Apr',
+  },
+  {
+    london: 57,
+    paris: 69,
+    newYork: 92,
+    seoul: 99,
+    month: 'May',
+  },
+  {
+    london: 60,
+    paris: 63,
+    newYork: 103,
+    seoul: 144,
+    month: 'June',
+  },
+  {
+    london: 59,
+    paris: 60,
+    newYork: 105,
+    seoul: 319,
+    month: 'July',
+  },
+  {
+    london: 65,
+    paris: 60,
+    newYork: 106,
+    seoul: 249,
+    month: 'Aug',
+  },
+  {
+    london: 51,
+    paris: 51,
+    newYork: 95,
+    seoul: 131,
+    month: 'Sept',
+  },
+  {
+    london: 60,
+    paris: 65,
+    newYork: 97,
+    seoul: 55,
+    month: 'Oct',
+  }
+]
+export function valueFormatter(value) {
+  return `${value}mm`;
+}
 const Dashboard = () => {
-  return <h1>Welcome to the Dashboard!</h1>;
+  return (
+    <>
+      <PieChart
+        series={[
+          {
+            data: desktopOS,
+            highlightScope: { fade: 'global', highlight: 'item' },
+            faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' },
+            valueFormatterPie,
+          },
+        ]}
+        height={200}
+      />
+
+
+      <BarChart
+        dataset={dataset}
+        xAxis={[{ scaleType: 'band', dataKey: 'month' }]}
+        series={[
+          { dataKey: 'london', label: 'London', valueFormatter },
+          { dataKey: 'paris', label: 'Paris', valueFormatter },
+          { dataKey: 'newYork', label: 'New York', valueFormatter },
+          { dataKey: 'seoul', label: 'Seoul', valueFormatter },
+        ]}
+        {...chartSetting}
+        />
+    </>
+  )
 };
 
 export default Dashboard;
