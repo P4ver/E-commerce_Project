b5c7a52790884ffa2e90c7bdf20c0e23443d586c
P4ver <s.pales20@gmail.com>
2024-10-17 17:00:53 +0100

    add page product

--- store/src/App.jsx
+++ store/src/App.jsx
@@ -3,6 +3,8 @@ import Home from './component/Home';
 import NavBare from './component/NavBare';
 import { BrowserRouter , Route, Routes } from 'react-router-dom';
 import './App.css'
+import ProductPage from './component/productPage';
+
 function App() {
   return (
     <>
@@ -10,6 +12,7 @@ function App() {
        <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/product/:id" element={<ProductPage />} />
         </Routes>
       </BrowserRouter>
 
