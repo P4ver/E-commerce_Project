3f861620961eadb60c93913941c9d5adc87e7073
P4ver <s.pales20@gmail.com>
2024-10-21 12:32:19 +0100

    update

--- store/src/component/categoryCard.jsx
+++ store/src/component/categoryCard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function CategoryCard({ name, onClick }) {
   return (
     <div 
-        className="category-card h-40 bg-gray-200 rounded-lg shadow-md flex items-center justify-center hover:bg-gray-300 transition-all duration-300 ease-in-out"
+        className="category-card h-40  shadow-md flex items-center justify-center hover:bg-gray-300 transition-all duration-300 ease-in-out"
         onClick={onClick}
         >
       <h2 className="text-lg font-semibold text-gray-700">{name}</h2>
