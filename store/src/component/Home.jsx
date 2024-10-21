3875e3cde6c8c15412a86b89d53320b7e278c04a
P4ver <s.pales20@gmail.com>
2024-10-21 12:27:31 +0100

    fix responisivity

--- store/src/component/Home.jsx
+++ store/src/component/Home.jsx
@@ -39,47 +39,54 @@ function Home() {
     setIsCategoryVisible((prev) => !prev); // Toggle the visibility
   };
   return (
-    <>
-      <div className='flex justify-center'>
+    <div className='bg-gray-50'>
+      <div className='flex justify-center w-3/4 mx-auto'>
         <Slide/>
         {/* <img className='w-96' src={delevry} alt="" /> */}
       </div>
       
-      {/* Button to toggle category visibility */}
-      <button 
-        onClick={toggleCategoryVisibility} 
-        className="mb-4 bg-blue-500 text-white px-4 py-2 rounded"
-      >
-        {isCategoryVisible ? 'Hide Categories' : 'Show Categories'}
-      </button>
-      <div className="home flex p-4">
-      {/* Sidebar for Categories */}
-      {isCategoryVisible && (
-        <div className="sidebar w-1/4 pr-4 max-h-screen sticky top-4 overflow-y-auto hide-scrollbar ">
-          <h2 className="text-xl font-bold mb-4">Categories</h2>
-          <div className="grid grid-cols-1 text-center md:grid-cols-2 gap-4 mb-8">
-            {categories.map((category) => (
-              <CategoryCard
-                key={category.id}
-                name={category.name}
-                onClick={() => handleCategoryClick(category.id)}
-              />
+      <div className="home p-4">
+        <div className='my-10'>
+          <div className='w-3/4 mx-auto'>
+            {/* Button to toggle category visibility */}
+            <button 
+              onClick={toggleCategoryVisibility} 
+              className="mb-4 bg-blue-500 text-white px-4 py-2 rounded "
+            >
+              {isCategoryVisible ? 'Hide Categories' : 'Show Categories'}
+            </button>
+          </div>
+          {/* Sidebar for Categories */}
+          {isCategoryVisible && (
+            <div className='w-3/4 mx-auto'>
+            {/* <div className="sidebar pr-4 max-h-screen sticky top-4 overflow-y-auto hide-scrollbar "> */}
+              <h2 className="text-xl font-bold mb-4 ">Categories</h2>
+              <div className="flex flex-wrap justify-center">
+                {categories.map((category) => (
+                  <div className='w-36 m-2 bg-white text-center'>
+                    <CategoryCard
+                      key={category.id}
+                      name={category.name}
+                      onClick={() => handleCategoryClick(category.id)}
+                    />
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
+
+        {/* Product Section */}
+        <div className="products w-3/4 mx-auto">
+          <h2 className="text-xl font-bold mb-4">Products</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+            {filteredProducts.map((product) => (
+              <ProductCard key={product.id} product={product} />
             ))}
           </div>
         </div>
-      )}
-
-      {/* Product Section */}
-      <div className="products w-3/4">
-        <h2 className="text-xl font-bold mb-4">Products</h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-          {filteredProducts.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-        </div>
       </div>
-      </div>
-    </>
+    </div>
   );
 }
 
